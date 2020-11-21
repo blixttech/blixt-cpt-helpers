15622c4f90c4ec992f9e0bcab8d73a8195d717f4
Kasun Hewage <kasun@blixt.tech>
2020-11-22 00:28:49 +0100

    Adding initial files.

---
+++ blixt/cpt_helpers.py
@@ -0,0 +1,68 @@
+from cpt.packager import ConanMultiPackager
+from cpt.ci_manager import CIManager
+from conans.client import conan_api
+from cpt.printer import Printer
+import os
+import sys
+import re
+import traceback
+import configparser
+
+
+def hidesensitive(output):
+    output_str = str(output)
+    output_str = re.sub(r'(CONAN_LOGIN_USERNAME[_\w+]*)=\"(\w+)\"', r'\1="xxxxxxxx"', output_str)
+    output_str = re.sub(r'(CONAN_PASSWORD[_\w+]*)=\"(\w+)\"', r'\1="xxxxxxxx"', output_str)
+    sys.stdout.write(output_str)
+
+
+def get_recipe_path(cwd=None):
+    conanfile = os.getenv("CONAN_CONANFILE", "conanfile.py")
+    if cwd is None:
+        return os.path.abspath(conanfile)
+    else:
+        return os.path.join(cwd, conanfile)
+
+
+def get_bool_from_env(var_name, default="1"):
+    val = os.getenv(var_name, default)
+    return str(val).lower() in ("1", "true", "yes", "y")
+
+
+def get_value_from_recipe(search_string, recipe=None):
+    if recipe is None:
+        recipe = get_recipe_path()
+    with open(recipe, "r") as conanfile:
+        contents = conanfile.read()
+        result = re.search(search_string, contents)
+    return result
+
+
+def inspect_value_from_recipe(attribute, recipe_path):
+    try:
+        conan_instance, _, _ = conan_api.Conan.factory()
+        inspect_result = conan_instance.inspect(path=recipe_path, attributes=[attribute])
+        return inspect_result.get(attribute)
+    except:
+        pass
+    return None
+
+
+def get_name_from_recipe(recipe=None):
+    name = inspect_value_from_recipe(attribute="name", recipe_path=get_recipe_path())
+    return name or get_value_from_recipe(r'''name\s*=\s*["'](\S*)["']''', recipe=recipe).groups()[0]
+
+
+def get_version_from_recipe(recipe=None):
+    version = inspect_value_from_recipe(attribute="version", recipe_path=get_recipe_path())
+    return version or get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''', recipe=recipe).groups()[0]
+
+
+def get_version_from_ci():
+    printer = Printer(hidesensitive)
+    ci_man = CIManager(printer)
+    return re.sub(".*/", "", ci_man.get_branch())
+
+
+def get_name_and_version():
+    return get_name_from_recipe(), get_version_from_recipe() or get_version_from_ci()
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blixt_cpt_helpers",
+    version="0.1.0",
+    author="Kasun Hewage",
+    author_email="kasun@blixt.tech",
+    description="Helper functions for CPT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blixttech/blixt-cpt-helpers",
+    packages=setuptools.find_packages(),
+    package_data={},
+    entry_points = {},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "conan-package-tools"
+    ],
+)
