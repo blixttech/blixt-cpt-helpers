e8d640bd125f6343d1043274303ea228f6affcc8
Kasun Hewage <kasun.ch@gmail.com>
2020-11-22 01:00:11 +0100

    Changed repository URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Helper functions for CPT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/blixttech/blixt-cpt-helpers",
+    url="https://github.com/blixttech/cpt-helpers",
     packages=setuptools.find_packages(),
     package_data={},
     entry_points = {},
